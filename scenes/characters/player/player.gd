09d06e180342eb60f1771c0ca7cd8ede50360619
Sases-Chaudhary <chaudharyniraj034@gmail.com>
2025-07-01 19:33:49 +0545

    Toolbar
    created simple toolbar

--- scenes/characters/player/player.gd
+++ scenes/characters/player/player.gd
@@ -1,5 +1,16 @@
 class_name Player
 extends CharacterBody2D
+
+@onready var hit_component: HitComponent = $HitComponent
+
 @export var current_tools: DataTypes.Tools = DataTypes.Tools.None
 
 var player_direction : Vector2
+
+func _ready() -> void:
+	ToolManager.tool_selected.connect(on_tool_selected)
+	
+func on_tool_selected(tool: DataTypes.Tools) -> void:
+	current_tools = tool
+	hit_component.current_tool = tool
+	print ( "Tool : " , tool)
