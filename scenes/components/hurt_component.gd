712c0a74be3a379ce6b8818a859ab459cc22538b
Sases-Chaudhary <chaudharyniraj034@gmail.com>
2025-11-19 22:23:18 +0545

    changes added
    Added Houses, NPC, dialogue, audio, menu

--- scenes/components/hurt_component.gd
+++ scenes/components/hurt_component.gd
@@ -8,7 +8,7 @@ signal  hurt
 func _on_area_entered(area: Area2D) -> void:
 	var hit_component = area as HitComponent
 	
-	print("Expected tool:", tool, "| Received tool:", hit_component.current_tool)
+	#print("Expected tool:", tool, "| Received tool:", hit_component.current_tool)
 	
 	if tool == hit_component.current_tool:
 		hurt.emit(hit_component.hit_damage)
